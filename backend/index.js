3fb9063b893a56588df5b7eff4798112f093dcaa
Salim Benamara <salim.d.benamara@gmail.com>
2025-08-04 23:42:25 +0200

    fix: Force Render rebuild - correct path issue

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-﻿// Render rebuild: 2025-08-04 23:06:54
+﻿// Render rebuild: 2025-08-04 23:42:25
 // backend/index.js - Point d'entrée minimal
 console.log('=== ECOLOJIA Backend Starting ===');
 console.log('Node version:', process.version);
@@ -6,4 +6,5 @@ console.log('Current directory:', __dirname);
 
 // Pas de require('zod') !
 require('./src/server.js');
+
 
