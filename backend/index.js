2fb50a98077590654ed153ffc854df804eddf1f6
Salim Benamara <salim.d.benamara@gmail.com>
2025-08-04 23:06:54 +0200

    fix: Force Render rebuild - quotaService sync issue

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-﻿// backend/index.js - Point d'entrée minimal
+﻿// Render rebuild: 2025-08-04 23:06:54
+// backend/index.js - Point d'entrée minimal
 console.log('=== ECOLOJIA Backend Starting ===');
 console.log('Node version:', process.version);
 console.log('Current directory:', __dirname);
 
 // Pas de require('zod') !
 require('./src/server.js');
+
